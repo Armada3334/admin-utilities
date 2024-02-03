c69aba583f77b55cb44549fbc19a064b7e2f9dbd
Armada <73616858+Armada3334@users.noreply.github.com>
2024-02-03 18:20:38 -0500

    Add OVA and VMDK support

--- proxmox/import-vmware.sh
+++ proxmox/import-vmware.sh
@@ -3,7 +3,7 @@
 # Get input from user flags for each variable
 while getopts "o:s:i:n:" flag; do
     case "${flag}" in
-        o) VMWARE_OVF_PATH=${OPTARG};;
+        o) VMWARE_FILE_PATH=${OPTARG};;
         s) PROXMOX_STORAGE=${OPTARG};;
         i) VM_ID=${OPTARG};;
         n) VM_NAME=${OPTARG};;
@@ -11,8 +11,8 @@ while getopts "o:s:i:n:" flag; do
 done
 
 # Prompt user for input if flags are not set
-if [[ -z $VMWARE_OVF_PATH ]]; then
-    read -p "Enter the VMware OVF path: " VMWARE_OVF_PATH
+if [[ -z $VMWARE_FILE_PATH ]]; then
+    read -p "Enter the VMware file path (OVF/OVA/VMDK): " VMWARE_FILE_PATH
 fi
 
 if [[ -z $PROXMOX_STORAGE ]]; then
@@ -27,17 +27,43 @@ if [[ -z $VM_NAME ]]; then
     read -p "Enter the new VM name: " VM_NAME
 fi
 
-# Convert VMware disk to QCOW2 format
-echo "Converting disk image..."
-qemu-img convert -f vmdk -O qcow2 "$(dirname "$VMWARE_OVF_PATH")/$(grep -oPm1 "(?<=<File ovf:href=\").+?(?=\" ovf:id=)" "$VMWARE_OVF_PATH")" "/var/lib/vz/images/$VM_ID/vm-$VM_ID-disk-1.qcow2"
+FILE_EXTENSION="${VMWARE_FILE_PATH##*.}"
+
+# Function to convert and import disk
+convert_and_import_disk() {
+    DISK_PATH="$1"
+    echo "Converting disk image..."
+    qemu-img convert -f vmdk -O qcow2 "${DISK_PATH}" "/var/lib/vz/images/$VM_ID/vm-$VM_ID-disk-1.qcow2"
+
+    echo "Importing disk to Proxmox VM..."
+    qm importdisk "$VM_ID" "/var/lib/vz/images/$VM_ID/vm-$VM_ID-disk-1.qcow2" "$PROXMOX_STORAGE" --format qcow2
+}
+
+case $FILE_EXTENSION in
+    ovf)
+        DISK_FILE="$(dirname "$VMWARE_FILE_PATH")/$(grep -oPm1 "(?<=<File ovf:href=\").+?(?=\" ovf:id=)" "$VMWARE_FILE_PATH")"
+        convert_and_import_disk "$DISK_FILE"
+        ;;
+    ova)
+        TEMP_DIR="/tmp/ova_extract_$VM_ID"
+        mkdir -p "$TEMP_DIR"
+        tar -xf "$VMWARE_FILE_PATH" -C "$TEMP_DIR"
+        DISK_FILE="$TEMP_DIR/$(ls $TEMP_DIR | grep -E '\.vmdk$')"
+        convert_and_import_disk "$DISK_FILE"
+        rm -rf "$TEMP_DIR"
+        ;;
+    vmdk)
+        convert_and_import_disk "$VMWARE_FILE_PATH"
+        ;;
+    *)
+        echo "Unsupported file format."
+        exit 1
+        ;;
+esac
 
 # Create a new VM in Proxmox
 echo "Creating new VM in Proxmox..."
 qm create "$VM_ID" --name "$VM_NAME" --memory 2048 --net0 virtio,bridge=vmbr0
-
-# Import the disk to Proxmox VM
-echo "Importing disk to Proxmox VM..."
-qm importdisk "$VM_ID" "/var/lib/vz/images/$VM_ID/vm-$VM_ID-disk-1.qcow2" "$PROXMOX_STORAGE" --format qcow2
 
 # Attach the disk to the VM
 echo "Attaching disk to VM..."
